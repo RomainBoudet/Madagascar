2fa8524992b04a57c5d69248b73d4f9fcca7ed02
RomainBoudet <romainoclock@gmail.com>
2021-07-24 11:26:05 +0200

    new model => _facture

---
+++ BACK/app/controllers/factureController.js
@@ -0,0 +1,202 @@
+const Facture = require('../models/facture');
+
+
+/**
+ * Une méthode qui va servir a intéragir avec le model Facture pour les intéractions avec la BDD
+ * Retourne un json
+ * @name factureController
+ * @method factureController
+ * @param {Express.Request} req - l'objet représentant la requête
+ * @param {Express.Response} res - l'objet représentant la réponse
+ * @return {JSON}  - une donnée en format json
+ */
+const factureController = {
+
+
+    getAll: async (req, res) => {
+        try {
+            const factures = await Facture.findAll();
+
+            res.status(200).json(factures);
+        } catch (error) {
+            console.trace('Erreur dans la méthode getAll du factureController :',
+                error);
+            res.status(500).json(error.message);
+        }
+    },
+
+    getOne: async (req, res) => {
+        try {
+
+            const facture = await Facture.findOne(req.params.id);
+            res.json(facture);
+
+        } catch (error) {
+            console.trace('Erreur dans la méthode getOne du factureController :',
+                error);
+            res.status(500).json(error.message);
+        }
+    },
+
+
+
+    getByIdClient: async (req, res) => {
+        try {
+            
+            const facture = await Facture.findByIdClient(req.params.id);
+            res.json(facture);
+
+        } catch (error) {
+            console.trace('Erreur dans la méthode getByIdFacture du factureController :',
+                error);
+            res.status(500).json(error.message);
+        }
+    },
+
+
+
+
+
+    new: async (req, res) => {
+        try {
+
+            const data = {};
+        
+            data.reference = req.body.reference;
+            data.montantHT = req.body.montantHT;
+            data.montantTTC = req.body.montantTTC;
+            data.montantTVA = req.body.montantTVA;
+            data.idPaiement = req.body.idPaiement;
+            data.idClient = req.body.idClient;
+
+            const newFacture = new Facture(data);
+            await newFacture.save();
+            res.json(newFacture);
+        } catch (error) {
+            console.log(`Erreur dans la méthode new du factureController : ${error.message}`);
+            res.status(500).json(error.message);
+        }
+    },
+
+    update: async (req, res) => {
+        try {
+
+            const {
+                id
+            } = req.params;
+            
+            const updateFacture = await Facture.findOne(id);
+
+
+            const reference = req.body.reference;
+            const montantHT = req.body.montantHT;
+            const montantTTC = req.body.montantTTC;
+            const montantTVA = req.body.montantTVA;
+            const idPaiement = req.body.idPaiement;
+            const idClient = req.body.idClient;
+        
+
+            let message = {};
+
+            if (reference) {
+                updateFacture.reference = reference;
+                message.reference = 'Votre nouveau reference a bien été enregistré ';
+            } else if (!reference) {
+                message.reference = 'Votre reference n\'a pas changé';
+            }
+
+
+            if (montantHT) {
+                updateFacture.montantHT = montantHT;
+                message.montantHT = 'Votre nouveau montantHT a bien été enregistré ';
+            } else if (!montantHT) {
+                message.montantHT = 'Votre nom de montantHT n\'a pas changé';
+            }
+
+
+            if (montantTTC) {
+                updateFacture.montantTTC = montantTTC;
+                message.montantTTC = 'Votre nouveau montantTTC a bien été enregistré ';
+            } else if (!montantTTC) {
+                message.montantTTC = 'Votre montantTTC n\'a pas changé';
+            }
+
+
+            if (montantTVA) {
+                updateFacture.montantTVA = montantTVA;
+                message.montantTVA = 'Votre nouveau montantTVA a bien été enregistré ';
+            } else if (!montantTVA) {
+                message.montantTVA = 'Votre montantTVA n\'a pas changé';
+            }
+
+            if (idPaiement) {
+                updateFacture.idPaiement = idPaiement;
+                message.idPaiement = 'Votre nouveau idPaiement a bien été enregistré ';
+            } else if (!idPaiement) {
+                message.idPaiement = 'Votre idPaiement n\'a pas changé';
+            }
+
+            if (idClient) {
+                updateFacture.idClient = idClient;
+                message.idClient = 'Votre nouveau idClient a bien été enregistré ';
+            } else if (!idClient) {
+                message.password = 'Votre idClient n\'a pas changé';
+            }
+
+             await updateFacture.update();
+            
+            res.json(message);
+
+        } catch (error) {
+            console.log(`Erreur dans la methode update du factureController ${error.message}`);
+            res.status(500).json(error.message);
+        }
+    },
+
+
+
+
+    delete: async (req, res) => {
+
+        try {
+
+            const factureInDb = await Facture.findOne(req.params.id);
+
+            const facture = await factureInDb.delete();
+
+            res.json(facture);
+
+        } catch (error) {
+            console.trace('Erreur dans la méthode delete du factureController :',
+                error);
+            res.status(500).json(error.message);
+        }
+    },
+
+
+    deleteByIdClient: async (req, res) => {
+
+        try {
+
+            const facturesInDb = await Facture.findByIdClient(req.params.id);
+            const arrayDeleted = [];
+            for (const factureInDb of facturesInDb) {
+
+                const factureHistoConn = await factureInDb.deleteByIdFacture();
+                arrayDeleted.push(factureHistoConn);
+            }
+
+
+            res.json(arrayDeleted[0]);
+
+        } catch (error) {
+            console.trace('Erreur dans la méthode deleteUByIdFacture du factureController :',
+                error);
+            res.status(500).json(error.message);
+        }
+    },
+
+
+}
+
+module.exports = factureController;
---
+++ BACK/app/models/facture.js
@@ -0,0 +1,219 @@
+const db = require('../database');
+const consol = require('../services/colorConsole');
+
+class Facture {
+
+    id;
+    reference;
+    dateFacturation;
+    montantHT;
+    montantTTC;
+    montantTVA;
+    updatedDate;
+    idPaiement;
+    idClient;
+
+
+    set date_facturation(val) {
+        this.dateFacturation = val;
+    }
+    set montant_HT(val) {
+        this.montantHT = val;
+    }
+    set montant_TTC(val) {
+        this.montantTTC = val;
+    }
+    set montant_TVA(val) {
+        this.montantTVA = val;
+    }
+    set updated_date(val) {
+        this.updatedDate = val;
+    }
+    set id_paiement(val) {
+        this.idPaiement = val;
+    }
+    set id_client(val) {
+        this.idClient = val;
+    }
+
+
+    /**
+     * @constructor
+     */
+    constructor(data = {}) {
+        for (const prop in data) {
+            this[prop] = data[prop];
+        }
+    }
+
+    /**
+     * Méthode chargé d'aller chercher les informations relatives à toutes les factures
+     * @returns - tous les factures présent en BDD
+     * @static - une méthode static
+     * @async - une méthode asynchrone
+     */
+    static async findAll() {
+        const {
+            rows
+        } = await db.query('SELECT * FROM mada.facture ORDER BY facture.id ASC');
+
+        if (!rows[0]) {
+            throw new Error("Aucunes factures dans la BDD");
+        }
+        consol.model(
+            `les informations des ${rows.length} factures ont été demandées !`
+        );
+
+        return rows.map((facture) => new Facture(facture));
+    }
+
+
+    /**
+     * Méthode chargé d'aller chercher les informations relatives à une facture via son id passée en paramétre
+     * @param - un id d'une facture
+     * @returns - les informations du facture demandées
+     * @static - une méthode static
+     * @async - une méthode asynchrone
+     */
+    static async findOne(id) {
+
+
+        const {
+            rows,
+        } = await db.query(
+            'SELECT * FROM mada.facture WHERE facture.id = $1;',
+            [id]
+        );
+
+        if (!rows[0]) {
+            throw new Error("Aucun facture avec cet id");
+        }
+
+        consol.model(
+            `le facture id : ${id} a été demandé en BDD !`
+        );
+
+        return new Facture(rows[0]);
+    }
+
+
+
+    /**
+     * Méthode chargé d'aller chercher les informations relatives à un idClient passé en paramétre
+     * @param idClient - un idClient d'une facture
+     * @returns - les informations d'une facture demandées
+     * @static - une méthode static
+     * @async - une méthode asynchrone
+     */
+    static async findByIdClient(idClient) {
+
+
+        const {
+            rows,
+        } = await db.query(
+            'SELECT * FROM mada.facture WHERE facture.id_client = $1;',
+            [idClient]
+        );
+
+        if (!rows[0]) {
+            throw new Error("Aucun facture avec cet idClient");
+        }
+
+        consol.model(
+            `la facture pour le idClient : ${idClient} a été demandé en BDD !`
+        );
+
+        return rows.map((id) => new Facture(id));
+    }
+
+    /**
+     * Méthode chargé d'aller insérer les informations relatives à une factures passé en paramétre
+     * @param reference - la reférence d'une facture
+     * @param montantHT - le montant HT d'una facture, 
+     * @param montantTTC - le montant TTC d'une facture,
+     * @param montantTVA - le montant TVA d'une facture
+     * @param idPaiement - l'identifiant d'un paiement
+     * @param idClient - l'id d'un client
+     * @returns - les informations du facture demandées
+     * @async - une méthode asynchrone
+     */
+    async save() {
+
+        const {
+            rows,
+        } = await db.query(
+            `INSERT INTO mada.facture (reference, montant_HT, montant_TTC, montant_TVA, id_paiement, id_client) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *;`,
+            [this.reference, this.montantHT, this.montantTTC, this.montantTVA, this.idPaiement, this.idClient]
+        );
+
+        this.id = rows[0].id;
+        this.createdDate = rows[0].date_facturation;
+        consol.model(
+            `le facture id ${this.id} a été inséré à la date du ${this.createdDate} pour le client id ${this.idClient} !`
+        );
+    }
+
+
+
+    /**
+     * Méthode chargé d'aller mettre à jour les informations relatives à un facture passé en paramétre
+     * @param reference - la reférence d'une facture
+     * @param montantHT - le montant HT d'una facture, 
+     * @param montantTTC - le montant TTC d'une facture,
+     * @param montantTVA - le montant TVA d'une facture
+     * @param idPaiement - l'identifiant d'un paiement
+     * @param idClient - l'id d'un client
+     * @param id - l'identifiant du champs a supprimer
+     * @returns - les informations du facture mis à jour
+     * @async - une méthode asynchrone
+     */
+    async updateByIdClient() {
+        const {
+            rows,
+        } = await db.query(
+            `UPDATE mada.facture SET reference = $1, montant_HT = $2, montant_TTC = $3, montant_TVA = $4, id_paiement = $5, id_client = $6  WHERE id_client = $7 RETURNING *;`,
+            [this.reference, this.montantHT, this.montantTTC, this.montantTVA, this.idPaiement, this.idClient, this.id]
+        );
+        this.updatedDate = rows[0].updated_date;
+        console.log(
+            `le facture du client id : ${this.idClient} comprenant le nouveau numéro ${this.adminTelephone} a été mise à jour le ${this.updatedDate} !`
+        );
+    }
+    /**
+     * Méthode chargé d'aller supprimer un facture passé en paramétre
+     * @param id - l'id d'un facture
+     * @async - une méthode asynchrone
+     */
+    async delete() {
+        const {
+            rows
+        } = await db.query('DELETE FROM mada.facture WHERE facture.id = $1 RETURNING *;', [
+            this.id,
+        ]);
+        consol.model(`le facture id ${this.id} a été supprimé !`);
+
+        return new Facture(rows[0]);
+    }
+
+    /**
+     * Méthode chargé d'aller supprimer une facture  via l'idClient passé en paramétre
+     * @param idClient - l'id d'un client
+     * @returns - les informations du facture qui viennent d'être supprimés.
+     * @async - une méthode asynchrone
+     */
+    async deleteByIdClient() {
+        const {
+            rows
+        } = await db.query('DELETE FROM mada.facture WHERE facture.id_client = $1 RETURNING *;', [
+            this.idClient
+        ]);
+        consol.model(`la facture du client id ${this.idClient} a été supprimé !`);
+
+        return rows.map((deletedClient) => new Facture(deletedClient));
+    }
+
+
+
+}
+
+module.exports = Facture;

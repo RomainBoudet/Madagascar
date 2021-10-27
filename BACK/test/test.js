9bbf7f024ee14ee6b4b382addf0e74d476199b9e
RomainBoudet <romainoclock@gmail.com>
2021-10-27 16:03:18 +0200

    first test on model

--- BACK/test/test.js
+++ BACK/test/test.js
@@ -1,3 +1,7 @@
+require('dotenv').config({
+    path: `${__dirname}/../.env.back`
+});
+
 const {
     expect
 } = require('chai');
@@ -9,7 +13,7 @@ const userLoginSchema = require('../app/schemas/userLoginSchema');
 
 // le mot de passe d'un utilisateur, doit avoir 8 caractéres au minimum, une lettre minuscule, une lettre majuscule, un nombre et un caractéres spécial parmis : (!@#$%^&*)
 
- let mockLogin;
+let mockLogin;
 
 describe(chalk.magenta('Test de validation du userLoginSchema :'), function () {
 
@@ -26,8 +30,6 @@ describe(chalk.magenta('Test de validation du userLoginSchema :'), function () {
         // validate sur un schema Joi retourne un objet avec systématiquement une propriétée value et en cas d'érreur, une propriété error 
         expect(userLoginSchema.validate(mockLogin)).not.to.have.property('error');
     });
-
-
 
 
     it("should not validate the password format (without special caracteres)", function () {
@@ -61,4 +63,38 @@ describe(chalk.magenta('Test de validation du userLoginSchema :'), function () {
     });
 
 
+});
+
+const db = require('../app/database');
+
+const Client = require('../app/models/client');
+
+const user ={};
+
+describe(chalk.yellow('Model Client'), function () {
+
+
+    before(async function () {
+
+        const {rows} = await db.query('INSERT INTO mada.client (prenom, nom_famille, email, password, id_privilege) VALUES ($1, $2, $3 ,$4, $5) RETURNING *;', ['Barack', 'Obama', 'obama@whiteHouse.us', '$2b$10$NTmOgZb2fN1QxewdWOYchOVQXUtUSaW47uOCBhRJ2zi1s2dK4kJsi', 1]);
+        user.id = rows[0].id;
+    });
+
+    after(async function () {
+
+        await db.query("DELETE FROM mada.client WHERE id = $1", [user.id])
+
+    });
+
+    describe('#findOne', function () {
+
+        it('should validate the findOne methode', function () {
+
+            it('should fetch an instance of Post', async function(){
+                const theClient = await Client.findOne(user.id);
+
+                expect (theClient).to.be.an.instanceOf(Client)
+            })
+        })
+    });
 });
